4ab74c78899c48b7a3f0a287500c68a3f1b5260e
James Gary <gmarchesi888@gmail.com>
2019-08-09 16:32:56 -0400

    Starting to pull auth functionality into App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
+
 import { Provider } from "react-redux";
 import store from "./store";
 import "./App.css";
@@ -8,6 +12,9 @@ import SideNav from "./components/layout/SideNav";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import PrivateRoute from "./components/private-route/PrivateRoute";
+import Dashboard from "./components/dashboard/Dashboard"
+
 
 class App extends Component {
   render() {
