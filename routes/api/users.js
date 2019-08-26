bc83336025e48bb6a9200a05027240dcf419c613
James Gary <gmarchesi888@gmail.com>
2019-08-26 16:14:14 -0400

    AAARRRGGHHH I left a require config in there. Now removed.

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const keys = require("../../config/keys");
+// const keys = require("../../config/keys");
 require("dotenv").config();
 
 // const isProduction = process.env.NODE_ENV === "production";
